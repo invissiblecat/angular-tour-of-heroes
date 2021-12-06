151637b5f1fa9c3ba3cd6722a95460ad4a948db5
Aleksandra Koroleva <aleksandra.koroleva@rsquad.io>
2021-12-06 20:56:42 +0300

    initial commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({ //class participates in dependency injection system
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('Hero Service: fetched heroes');
+    return heroes;
+  }
+}
